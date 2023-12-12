ad4ccb911f3d8824ebd218d47734e8ba0a48a0a8
5im0ne1997 <bonfasimone@gmail.com>
2023-12-12 22:12:00 +0100

    day 2 solution 1 and 2

--- 2023/day_2.py
+++ 2023/day_2.py
@@ -3,6 +3,7 @@ import re
 
 path = pathlib.Path("2023", "day_2.txt")
 sum1 = 0
+sum2=0
 game_counter = 0
 
 max_cubes = {"red": 12, "green": 13, "blue": 14}
@@ -11,19 +12,19 @@ with open(path, "rt") as file:
     for line in file:
         game_counter += 1
         sub_games = re.search(":(.*)", line).group(1)
+        game_max_cubes={"red": 0, "green": 0, "blue": 0}
         for sub_game_cubes in sub_games.split(";"):
-            fail=False
             for cubes in sub_game_cubes.split(","):
                 cubes_number = re.search(r"(\d+) (\w+)", cubes)
-                number = cubes_number.group(1)
+                number = int(cubes_number.group(1))
                 color = cubes_number.group(2)
-                if (int(number) > int(max_cubes[color])):
-                    fail = True
-                    break
-            if fail:
-                break
-        if not fail:
-            print(game_counter)
+                if (number > game_max_cubes[color]):
+                    game_max_cubes[color] = number
+        if (game_max_cubes["red"] <= max_cubes["red"]) and  (game_max_cubes["green"] <= max_cubes["green"]) and (game_max_cubes["blue"] <= max_cubes["blue"]):
             sum1 += game_counter
+        sum2 += (game_max_cubes["red"] * game_max_cubes["green"]) * game_max_cubes["blue"]
+        
+
 
 print(sum1)
+print(sum2)
