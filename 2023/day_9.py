d7a04a37147f83de7c37a84e017b435c3c294a29
5im0ne1997 <bonfasimone@gmail.com>
2024-01-09 20:14:42 +0100

    day 9 part 1 e 2

--- 2023/day_9.py
+++ 2023/day_9.py
@@ -1,14 +1,34 @@
 from pathlib import Path
+from itertools import pairwise
 
 class Game:
 
-    def __init__(self, input):
+    def __init__(self, input, ispart2):
         with open(input) as file:
             self.all_lines = [[number for number in line.split(" ")] for line in file.read().split("\n")]
-        
-        
+        self.sum = 0
+        self.ispart2 = ispart2
+
+    def solution(self):
+        for line in self.all_lines:
+            if self.ispart2:
+                line.reverse()
+            flag = True
+            while flag:
+                newline = list()
+                self.sum += int(line[-1])
+                for a,b in list(pairwise(line)):
+                    newline.append(int(b) - int(a))
+                
+                test = 0 in set(newline)
+                if len(set(newline)) == 1 and 0 in set(newline):
+                    flag = False
+                line = newline
+        print(self.sum)
 
 inputfile = Path("2023", "day_9.txt")
-day9part1 = Game(inputfile)
+day9part1 = Game(inputfile, False)
+day9part1.solution()
 
-pass
+day9part2 = Game(inputfile, True)
+day9part2.solution()
