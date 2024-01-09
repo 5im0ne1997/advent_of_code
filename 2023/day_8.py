86874e00f11566d05b46c150f95fd87fed20c7fa
5im0ne1997 <bonfasimone@gmail.com>
2024-01-09 12:44:15 +0100

    part 1 e 2 solution

--- 2023/day_8.py
+++ 2023/day_8.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 import re
 from collections import defaultdict
+from math import lcm
 
 class Game:
 
     def __init__(self, input, ispart2):
-        self.result = 0
+        self.result = defaultdict(list)
         self.ispart2 = ispart2
+        self.startpositions = list()
         with open(input) as file:
             all_line = file.read()
         self.instructions, map = all_line.split("\n\n")
@@ -17,24 +19,50 @@ class Game:
             rl = re.findall(r"\w+", rl)
             self.map[start] = (rl[0], rl[1])
 
+    def findallstartpositions(self):
+        if self.ispart2:
+            for step in self.map.keys():
+                if step.endswith("A"):
+                    self.startpositions.append(step)
+
     def solution(self):
-        position = 'AAA'
-        while position != 'ZZZ':
-            for i in self.instructions:
-                self.result += 1
-                if i == 'L':
-                    step = 0
-                else:
-                    step = 1
-                position = self.map[position][step]
-                if position == 'ZZZ':
+        if self.ispart2:
+            self.findallstartpositions()
+        else:
+            self.startpositions.append('AAA')
+        self.startpositions
+        for index, position in enumerate(self.startpositions):
+            self.result[index] = 0
+            while True: #position != 'ZZZ':
+                if self.ispart2 and position.endswith("Z"):
                     break
-        return self.result
+                elif position == "ZZZ":
+                    break
+                for i in self.instructions:
+                    self.result[index] += 1
+                    if i == 'L':
+                        step = 0
+                    else:
+                        step = 1
+                    position = self.map[position][step]
+                    if self.ispart2 and position.endswith("Z"):
+                        break
+                    elif position == "ZZZ":
+                        break
+        if self.ispart2:
+            results = list(self.result.values())
+            return lcm(*results)
+        else:
+            return self.result[0]
         
 
 
 inputfile = Path("2023", "day_8.txt")
+
 day8part1 = Game(inputfile, False)
 print(day8part1.solution())
+
+day8part2 = Game(inputfile, True)
+print(day8part2.solution())
 
 pass
