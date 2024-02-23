2af86846c22218ea28854ad940ca7b3b92314614
5im0ne1997 <bonfasimone@gmail.com>
2024-02-23 12:20:20 +0100

    part 2 day 12

--- 2023/day_12.py
+++ 2023/day_12.py
@@ -1,59 +1,55 @@
 from pathlib import Path
-from copy import deepcopy
+from functools import cache
 
-row = 0
-
-def resolve(ds, dg):
-    ds = ds
-    dg = dg
-    solution = {}
-    if len(solution) == 0:
-        #solution = {("matching string", index dg, number of #): "previous c"}
-        solution = {(".", 0, 0, 0): "."}
-    for c in ds:
-        temp = deepcopy(solution)
-        for s in temp.items():
-            #s = ((".", 0, 0), ".")
-            if c == "?" and s[1][0] == '.':
-                del solution[s[0]]
-                solution[(f"{s[0][0]}#", s[0][1] + 1, 1)] = '#'
-                solution[(f"{s[0][0]}.", s[0][1], s[0][2])] = '.'
-            elif c == "?" and s[1][0] == '#':
-                del solution[s[0]]
-                if s[0][1] <= len(dg):
-                    if s[0][2] == int(dg[s[0][1]-1]):
-                        solution[(f"{s[0][0]}.", s[0][1], 0)] = '.'
-                    elif s[0][2] <= int(dg[s[0][1]-1]):
-                        solution[(f"{s[0][0]}#", s[0][1], s[0][2] +1 )] = '#'
-            elif c == "#" and s[1][0] == '.':
-                del solution[s[0]]
-                solution[(f"{s[0][0]}#", s[0][1] + 1, 1)] = '#'
-            elif c == "#" and s[1][0] == '#':
-                del solution[s[0]]
-                if s[0][1] <= len(dg):
-                    if s[0][2] <= int(dg[s[0][1]-1]):
-                        solution[(f"{s[0][0]}#", s[0][1], s[0][2] +1 )] = '#'
-            elif c == '.' and s[1][0] == '.':
-                del solution[s[0]]
-                solution[(f"{s[0][0]}.", s[0][1], s[0][2])] = '.'
-            elif  c == '.' and s[1][0] == '#':
-                del solution[s[0]]
-                if s[0][1] <= len(dg):
-                    if s[0][2] == int(dg[s[0][1] - 1]):
-                        solution[(f"{s[0][0]}.", s[0][1], 0)] = '.'
-    global row
-    row += 1
-    print(f"RIGA {row}: {len(solution)}")
-    return len(solution)
+@cache
+def resolve(ds, dg, previous, index_dg, hash_number, index):
+    sum = 0
+    dg1 = dg.split(",")
+    if index < len(ds):
+        c = ds[index]
+        if c == "?" and previous == '.':
+            if index_dg < len(dg1):
+                sum += resolve(ds, dg, "#", index_dg, 1, index +1)
+            sum += resolve(ds, dg, ".", index_dg, hash_number, index +1)
+        elif c == "?" and previous == '#':
+            if hash_number+1 <= int(dg1[index_dg]):
+                sum += resolve(ds, dg, "#", index_dg, hash_number + 1, index +1)
+            if index_dg < len(dg1) and hash_number == int(dg1[index_dg]):
+                sum += resolve(ds, dg, ".", index_dg + 1, 0, index +1)
+        elif c == "#" and previous == '.':
+            if index_dg < len(dg1):
+                sum += resolve(ds, dg, "#", index_dg, 1, index +1)
+        elif c == "#" and previous == '#':
+            if hash_number+1 <= int(dg1[index_dg]):
+                sum += resolve(ds, dg, "#", index_dg, hash_number + 1, index +1)
+        elif c == '.' and previous == '.':
+            sum += resolve(ds, dg, ".", index_dg, hash_number, index +1)
+        elif  c == '.' and previous == '#':
+            if index_dg < len(dg1) and hash_number == int(dg1[index_dg]):
+                sum += resolve(ds, dg, ".", index_dg + 1, 0, index +1)
+    elif index_dg == len(dg1):
+        return 1
+    return sum
                     
-
-
 sum1 = 0
+sum2 = 0
+row = 0
 with open(Path("2023","day_12.txt")) as file:
     for line in file.read().split("\n"):
+        row += 1
         ds, dg = line.split(" ")
-        ds = f"{ds}."
-        dg = dg.split(",")
-        sum1 += resolve(ds, dg)
+        for part2 in [False, True]:
+            print(f"RIGA {row}")
+            if part2:
+                ds = f"{ds}?{ds}?{ds}?{ds}?{ds}"
+                dg = f"{dg},{dg},{dg},{dg},{dg}"
+                ds2 = f"{ds}."
+                sum2 += resolve(ds2, dg, '.', 0, 0, 0)
+            else:
+                ds1 = f"{ds}."
+                sum1 += resolve(ds1, dg, '.', 0, 0, 0)
+        
         pass
-    print(sum1)
+
+print(sum1)
+print(sum2)
