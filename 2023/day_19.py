468f43e9683854f75867e0b47710677126c0eed3
5im0ne1997 <bonfasimone@gmail.com>
2024-03-13 09:15:41 +0100

    day 19 part 1 and part 2

--- 2023/day_19.py
+++ 2023/day_19.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from copy import deepcopy
+from copy import copy
 
 class WorkFlow():
     def __init__(self, workflows: str) -> None:
@@ -7,7 +7,7 @@ class WorkFlow():
         self.exit = self.conditions[-1]
         self.conditions.pop()
 
-    def test(self, machine_parts: object, part2: bool = False) -> str:
+    def test(self, machine_parts: object) -> str:
         for single_condition in self.conditions:
             condition, exit = single_condition.split(":")
             if '<' in condition:
@@ -20,6 +20,42 @@ class WorkFlow():
                     return exit
         else:
             return self.exit
+    
+    def test2(self, machine_parts: object) -> list:
+        part2_to_test = [machine_parts]
+        part2_tested = []
+        for single_condition in self.conditions:
+            temp = []
+            for to_test in part2_to_test:
+                condition, exit = single_condition.split(":")
+                if '<' in condition:
+                    category, number = condition.split("<")
+                    before = [to_test.machine_part[category][0], min(to_test.machine_part[category][1], int(number) - 1)]
+                    after = [max(to_test.machine_part[category][0], int(number)), to_test.machine_part[category][1]]
+                    if before[1] >= before[0]:
+                        part2_tested.append(Machine_Parts2(to_test.machine_part))
+                        part2_tested[-1].machine_part[category] = before
+                        part2_tested[-1].status = exit
+                    if after[1] >= after[0]:
+                        temp.append(Machine_Parts2(to_test.machine_part))
+                        temp[-1].machine_part[category] = after
+                elif '>' in condition:
+                    category, number = condition.split(">")
+                    before = [to_test.machine_part[category][0], min(to_test.machine_part[category][1], int(number))]
+                    after = [max(to_test.machine_part[category][0], int(number) + 1), to_test.machine_part[category][1]]
+                    if before[1] >= before[0]:
+                        temp.append(Machine_Parts2(to_test.machine_part))
+                        temp[-1].machine_part[category] = before
+                    if after[1] >= after[0]:
+                        part2_tested.append(Machine_Parts2(to_test.machine_part))
+                        part2_tested[-1].machine_part[category] = after
+                        part2_tested[-1].status = exit
+            part2_to_test = copy(temp)
+        else:
+            for to_test in part2_to_test:
+                to_test.status = self.exit
+                part2_tested.append(to_test)
+            return part2_tested
 
 class Machine_Parts():
     def __init__(self, machine_parts: str) -> None:
@@ -34,7 +70,15 @@ class Machine_Parts():
 
 class Machine_Parts2():
     def __init__(self, machine_parts: dict):
-        self.machine_part = deepcopy(machine_parts)
+        self.machine_part = copy(machine_parts)
+        self.status = "in"
+
+    def solution(self) -> int:
+        dx = self.machine_part['x'][1] - self.machine_part['x'][0] + 1
+        dm = self.machine_part['m'][1] - self.machine_part['m'][0] + 1
+        da = self.machine_part['a'][1] - self.machine_part['a'][0] + 1
+        ds = self.machine_part['s'][1] - self.machine_part['s'][0] + 1
+        return (dx * dm * da * ds)
 
 with open(Path("2023","day_19.txt")) as file:
     workflows, machines_parts = file.read().split("\n\n")
@@ -66,4 +110,24 @@ for machine_parts in all_machines_parts:
 
 print(sum1)
 
+sum2 = 0
+to_test_part2 = [Machine_Parts2({'x':[1,4000],'m':[1,4000],'a':[1,4000],'s':[1,4000]})]
+
+tested_part2 = []
+while len(to_test_part2) > 0:
+    all_machines_parts_2 = copy(to_test_part2)
+    to_test_part2 = []
+    for machine_parts_2 in all_machines_parts_2:
+        temp_machine_parts_2 = all_workflows[machine_parts_2.status].test2(machine_parts_2)
+        for machine_parts in temp_machine_parts_2:
+            if machine_parts.status == 'A':
+                tested_part2.append(machine_parts)
+            elif machine_parts.status != 'R':
+                to_test_part2.append(machine_parts)
+
+
+for part2 in tested_part2:
+    sum2 += part2.solution()
+
+print(sum2)
 pass
