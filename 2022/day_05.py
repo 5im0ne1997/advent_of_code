c02f7160c373012d8098d07de851f00d710d5be3
5im0ne1997 <bonfasimone@gmail.com>
2024-03-29 19:09:17 +0100

    day 05 2022 part 2

--- 2022/day_05.py
+++ 2022/day_05.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from itertools import batched
 from collections import defaultdict
+from copy import deepcopy
 
 with open(Path("2022","day_05.txt")) as file:
     stack_input, move_input = file.read().split("\n\n")
@@ -18,16 +19,30 @@ for line in stacks_temp:
             crates[t].append(crate[1])
         t += 1
 
+crates_2 = deepcopy(crates)
+
 for move in moves:
     number = int(move[1])
     move_from = int(move[3])
     move_to  = int(move[5])
-    for n in range(number):
-        crates[move_to].append(crates[move_from].pop())
-step1 = ''
+    first_part, second_part = [crates[move_from][:len(crates[move_from]) - number], crates[move_from][len(crates[move_from]) - number:]]
+    first_part_2, second_part_2 = [crates_2[move_from][:len(crates_2[move_from]) - number], crates_2[move_from][len(crates_2[move_from]) - number:]]
+    crates[move_from] = first_part
+    crates_2[move_from] = first_part_2
+    if len(second_part) > 1:
+        second_part.reverse()
+    crates[move_to] = crates[move_to] + second_part
+    crates_2[move_to] = crates_2[move_to] + second_part_2
+    
+
+
+part1 = ''
+part2 = ''
 
 for crate in crates.values():
-    step1 = f"{step1}{crate[-1]}"
+    part1 = f"{part1}{crate[-1]}"
+for crate in crates_2.values():
+    part2 = f"{part2}{crate[-1]}"
 
-print(step1)
-pass
+print(part1)
+print(part2)
