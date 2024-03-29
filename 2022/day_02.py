7679ce811d9eefc0a2d15d752687547284742a3b
5im0ne1997 <bonfasimone@gmail.com>
2024-03-29 09:17:53 +0100

    day 02 2022 part 2

--- 2022/day_02.py
+++ 2022/day_02.py
@@ -3,37 +3,50 @@ from pathlib import Path
 with open(Path("2022","day_02.txt")) as file:
     input_file = [game for game in file.read().split("\n")]
 
-shape_to_type= {
-    'A': ['rock', 1],
-    'B': ['paper', 2],
-    'C': ['scissors', 3],
-    'X': ['rock', 1],
-    'Y': ['paper', 2],
-    'Z': ['scissors', 3]
+shape_to_type = {
+    'A': 'rock',
+    'B': 'paper',
+    'C': 'scissors',
+    'X': ['rock', 0],
+    'Y': ['paper', 3],
+    'Z': ['scissors', 6]
+}
+
+shape_to_point = {
+    'rock': 1,
+    'paper': 2,
+    'scissors': 3
 }
 
 win_point = {
     'rock': {
         'rock': 3,
-        'paper': 0,
-        'scissors': 6
-    },
-    'paper':{
-        'rock': 6,
-        'paper': 3,
+        'paper': 6,
         'scissors': 0
     },
-    'scissors':{
+    'paper':{
         'rock': 0,
-        'paper': 6,
+        'paper': 3,
+        'scissors': 6
+    },
+    'scissors':{
+        'rock': 6,
+        'paper': 0,
         'scissors': 3
     }
 }
+
 part1 = 0
+part2 = 0
 for game in input_file:
     opponent, you = game.split(" ")
     opponent_shape = shape_to_type[opponent]
     your_shape = shape_to_type[you]
-    part1 += your_shape[1] + win_point[your_shape[0]][opponent_shape[0]]
+    part1 += shape_to_point[your_shape[0]] + win_point[opponent_shape][your_shape[0]]
+
+    for shape_2 in win_point[opponent_shape].items():
+        if your_shape[1] == shape_2[1]:
+            part2 += shape_to_point[shape_2[0]] + shape_2[1]
 
 print(part1)
+print(part2)
