fa0e60d1d01984e22bb1f644305cdc61abc6034a
5im0ne1997 <bonfasimone@gmail.com>
2024-08-06 16:04:56 +0200

    delete day 21 wrng file, modify day 20 with solution part 1 and test part 2

--- 2023/day_20.py
+++ 2023/day_20.py
@@ -1,102 +1,172 @@
 from pathlib import Path
 from abc import ABC, abstractmethod
-from copy import copy, deepcopy
+from copy import copy
 
 class Module(ABC):
-    def __init__(self, output: list) -> None:
-        self.output = output
-        self.pulse = 'low'
-        self.input = []
-        self.last = []
 
-    def send(self) -> None:
-        queue = []
-        for module in self.output:
-            queue.append((self.pulse, module))
-        return queue
+    def __init__(self, name: str):
+        self.name = name
+        self.linked_to = []
+        self.low_sended = 0
+        self.high_sended = 0
+        self.can_send_pulse = True
     
+    def add_link_to(self, module: object):
+        self.linked_to.append(module)
+        module.add_link_from(self.name)
+
     @abstractmethod
-    def receive(self, pulse: str, module: str):
+    def add_link_from(self, name: str):
         pass
+
+    @abstractmethod
+    def recive_pulse(self, pulse: bool, name: str):
+        pass
+
+    @abstractmethod
+    def send_pulses(self):
+        pass
+    
+class FlipFlop(Module):
+
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.state = False #Flase is Off and send a low pulse (False), True is On and send high pulse (True)
+    
+    def add_link_from(self, name: str):
+        pass #Not necessary
+
+    def recive_pulse(self, pulse: bool, name: str):
+        if not pulse: #If recive a Low Pulse
+            self.state = not self.state #Switch state On or Of
+            self.can_send_pulse = True #can send pulse in next step
+        else:
+            self.can_send_pulse = False #If recive an High Pulse can't send pulse in next step
+
+    def send_pulses(self):
+        for module in self.linked_to:
+            module.recive_pulse(self.state, self.name)
+            if self.state:
+                self.high_sended += 1
+            else:
+                self.low_sended += 1
+        
+class Conjunction(Module):
+    
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.linked_from = {}
+    
+    def add_link_from(self, name: str):
+        self.linked_from[name] = False #Necessary for remembering all recived pulses
+    
+    def recive_pulse(self, pulse: bool, name: str):
+        self.linked_from[name] = pulse #Save last recived pulse
+    
+    def send_pulses(self):
+        pulse = False #Send Low pulse if all remembered recived pulses are High
+        for remember in self.linked_from.values():
+            if not remember:
+                pulse = not pulse #Send High pulse if at least one remembered recived pulses are Low
+                break
+        for module in self.linked_to:
+            module.recive_pulse(pulse, self.name)
+            if pulse:
+                self.high_sended += 1
+            else:
+                self.low_sended += 1
+                print(f"{self.name} {self.linked_from}")
 
 class Broadcaster(Module):
-    def __init__(self, output: list) -> None:
-        super().__init__(output)
 
-    def receive(self, pulse: str, module: str):
-        pass
+    def add_link_from(self, name: str):
+        pass #Not necessary
 
-class Flip_Flop(Module):
-    def __init__(self, output: list) -> None:
-        super().__init__(output)
-        self.state = 'off'
+    def recive_pulse(self, pulse: bool, name: str):
+        pass #Not necessary
 
-    def receive(self, pulse: str, module: str):
-        if pulse == 'low':
-            match self.state:
-                case 'on':
-                    self.state = 'off'
-                    self.pulse = 'low'
-                case 'off':
-                    self.state = 'on'
-                    self.pulse = 'high'
-            return self.send()
+    def send_pulses(self):
+        for module in self.linked_to:
+            module.recive_pulse(False, self.name) #Button send low pulse (False) and broadcaster always send low pulse (False)
+            self.low_sended += 1
 
-class Conjunction(Module):
-    def __init__(self, output: list) -> None:
-        super().__init__(output)
-        self.pulse = 'high'
+class EndModule(Module):
 
-    def receive(self, pulse: str, module: str):
-        self.last[self.input.index(module)] = pulse
-        if 'low' in self.last:
-            self.pulse = 'high'
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.low_pulse_recived = 0 #If this module is RX, print solution when it recives a low pulse
+
+    def add_link_from(self, name: str):
+        pass #Not necessary
+
+    def recive_pulse(self, pulse: bool, name: str):
+        if not pulse:
+            self.low_pulse_recived += 1
+
+    def send_pulses(self):
+        pass #Not necessary
+
+if __name__ == "__main__":
+
+    with open(Path("2023","day_20.txt")) as input_file:
+        all_comunication = [row for row in input_file.read().split("\n")]
+    
+    all_modules = {}
+
+    for module in all_comunication:
+        name, linked_to = module.split(" -> ")
+        if name == "broadcaster":
+            all_modules[name] = Broadcaster(name)
         else:
-            self.pulse = 'low'
-        return self.send()
+            module_type = name[:1]
+            name = name[1:]
+            if module_type == '%':
+                all_modules[name] = FlipFlop(name)
+            else:
+                all_modules[name] = Conjunction(name)
+        all_modules[name].temp_link = linked_to
+    
+    temp_end_module = {}
 
-with open(Path("2023","day_20.txt")) as file:
-    input_file = [line for line in file.read().split("\n")]
+    for module in all_modules.values():
+        linked_to = module.temp_link.split(", ")
+        for link in linked_to:
+            if link in all_modules:
+                module.add_link_to(all_modules[link])
+            else:
+                temp_end_module[link] = EndModule(link)
+                module.add_link_to(temp_end_module[link])
+        del module.temp_link
+    for name, module in temp_end_module.items():
+        all_modules[name] = module
+    del temp_end_module
+    
+    all_modules['button'] = Broadcaster("button")
+    all_modules['button'].add_link_to(all_modules['broadcaster'])
 
-circuit = {}
-for line in input_file:
-    start, end = line.split(" -> ")
-    end = end.split(", ")
-    if start == "broadcaster":
-        circuit[start] = Broadcaster(end)
-    elif start.startswith("%"):
-        start = start.lstrip("%")
-        circuit[start] = Flip_Flop(end)
-    elif start.startswith("&"):
-        start = start.lstrip("&")
-        circuit[start] = Conjunction(end)
+    counter = 0
+    while counter < 1000:
+        queue = [all_modules["button"]]
+        counter += 1
+        while len(queue) > 0:
+            temp_queue = []
+            for pulse_to_send in queue:
+                pulse_to_send.send_pulses()
+                if all_modules['rx'].low_pulse_recived == 1:
+                    sum_low_2 = 0
+                    sum_high_2 = 0
+                    for module in all_modules.values():
+                        sum_low_2 += module.low_sended
+                        sum_high_2 += module.high_sended
+                for sender in pulse_to_send.linked_to:
+                    if sender.can_send_pulse:
+                        temp_queue.append(sender)
+            queue = copy(temp_queue)
 
-for module_name, module_object in circuit.items():
-    for output in module_object.output:
-        if output in circuit:
-            circuit[output].input.append(module_name)
-            circuit[output].last.append('low')
-
-
-pulses = {"low": 0, "high": 0}
-
-for i in range(1000):
-    to_send = {"broadcaster": circuit["broadcaster"].send()}
-    pulses["low"] += 1
-    while len(to_send) > 0:
-        temp_to_send = {}
-        for sender, output in to_send.items():
-            for module in output:
-                pulses[module[0]] += 1
-                if module[1] in circuit:
-                    temp_queue = (circuit[module[1]].receive(module[0], sender))
-                    if temp_queue != None:
-                        new_sender = module[1]
-                        queue = []
-                        for pulse in temp_queue:
-                            queue.append(pulse)
-                        temp_to_send[new_sender] = copy(queue)
-        to_send = deepcopy(temp_to_send)
-
-print(pulses['low'] * pulses['high'])
-pass
+    sum_low_1 = 0
+    sum_high_1 = 0
+    for module in all_modules.values():
+        sum_low_1 += module.low_sended
+        sum_high_1 += module.high_sended
+    print(f"Solution 1: {sum_low_1 * sum_high_1}")
+    print(f"Solution 2: {sum_low_2 * sum_high_2}")
