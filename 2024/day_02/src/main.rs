cb7dd03bada7eb09502ab0338136aeac779ecf4a
5im0ne1997 <bonfasimone@gmail.com>
2024-12-23 20:35:55 +0100

    Prepare to part 2

--- 2024/day_02/src/main.rs
+++ 2024/day_02/src/main.rs
@@ -6,34 +6,43 @@ fn main() {
     let input_file = BufReader::new(File::open("./src/input.txt").expect("Fail to read file"));
     let mut safe_report: u32 = 0;
     for line in input_file.lines() {
-        let mut previous_number: u32 = 0;
-        let mut up = false;
-        let mut down = false;
-        let mut flag = true;
-        for (i, number) in line.unwrap().split_whitespace().enumerate() {
-            let current_number: u32 = number.parse().unwrap();
-            if i == 0 {
-                previous_number = current_number;
-            } else {
-                let sum = current_number as f32 - previous_number as f32;
-                if sum > 0 as f32 && sum < 4 as f32 {
-                    up = true;
-                } else if sum < 0 as f32 && sum > -4 as f32 {
-                    down = true;
-                } else {
-                    flag = false;
-                    break;
-                }
-            }
+        let mut list_number: Vec<u32> = Vec::new();
+        for number in line.unwrap().split_whitespace() {
+            list_number.push(number.parse().unwrap());
+        }
+        if resolve(list_number) {
+            safe_report += 1;
+        }
+    }
+
+    println!("{safe_report}");
+}
+
+fn resolve(list_number: Vec<u32>) -> bool {
+    let mut previous_number: u32 = 0;
+    let mut up = false;
+    let mut down = false;
+    let mut flag = true;
+    for (i, number) in list_number.into_iter().enumerate() {
+        let current_number: u32 = number;
+        if i == 0 {
             previous_number = current_number;
-            if up && down {
+        } else {
+            let sum = current_number as f32 - previous_number as f32;
+            if sum > 0 as f32 && sum < 4 as f32 {
+                up = true;
+            } else if sum < 0 as f32 && sum > -4 as f32 {
+                down = true;
+            } else {
                 flag = false;
                 break;
             }
         }
-        if flag {
-            safe_report += 1;
+        previous_number = current_number;
+        if up && down {
+            flag = false;
+            break;
         }
     }
-    println!("{safe_report}");
+    return flag;
 }
