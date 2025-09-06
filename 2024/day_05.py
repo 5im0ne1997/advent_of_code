3eaed863c890bbb4c0372386d709faf262de9552
5im0ne1997 <bonfasimone@gmail.com>
2025-09-06 18:20:33 +0200

    update solution 1 day 5 2024

--- 2024/day_05.py
+++ 2024/day_05.py
@@ -1,21 +1,22 @@
 from collections import defaultdict
 
-if __name__ == "__main__":
-    with open("input.txt", mode="r", encoding="UTF-8") as file:
-        first_part, second_part = file.read().strip().split("\n\n")
-        ordening_rules = defaultdict(list)
-        for line in first_part.splitlines():
-            a, b = line.split("|")
-            ordening_rules[a].append(b)
-        page_update = [list(line.split(",")) for line in second_part.splitlines()]
-    sum_1 = 0
-    for update in page_update:
-        correct_update = True
-        for index, page in enumerate(update):
-            if page in ordening_rules:
-                for related_page in ordening_rules[page]:
-                    if related_page in update[:index]:
-                        correct_update = False
-        if correct_update:
-            sum_1 += int(update[(len(update) // 2)])
-    print(f"Solution 1 is {sum_1}")
+def check_correct_update(update: list[str]):
+    for index, page in enumerate(update):
+        if page in ORDENING_RULES:
+            for related_page in ORDENING_RULES[page]:
+                if related_page in update[:index]:
+                    return False
+    return True
+
+with open("input.txt", mode="r", encoding="UTF-8") as file:
+    first_part, second_part = file.read().strip().split("\n\n")
+    ORDENING_RULES = defaultdict(list)
+    for line in first_part.splitlines():
+        a, b = line.split("|")
+        ORDENING_RULES[a].append(b)
+    PAGE_UPDATE = [list(line.split(",")) for line in second_part.splitlines()]
+SUM_1 = 0
+for update in PAGE_UPDATE:
+    if check_correct_update(update):
+        SUM_1 += int(update[(len(update) // 2)])
+print(f"Solution 1 is {SUM_1}")
