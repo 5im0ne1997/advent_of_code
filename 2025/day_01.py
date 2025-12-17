fc0da8c987b0a8961a27793bc66a650f70e4b41b
5im0ne1997 <bonfasimone@gmail.com>
2025-12-17 10:36:48 +0100

    try solution 2 day 1 2025

--- 2025/day_01.py
+++ 2025/day_01.py
@@ -1,5 +1,5 @@
 import pathlib
-import copy
+from copy import copy
 
 with open(pathlib.Path('2025','day_01.txt')) as file:
     input_file = file.read().splitlines()
@@ -7,15 +7,22 @@ with open(pathlib.Path('2025','day_01.txt')) as file:
 moves = [(line[:1],int(line[1:])) for line in input_file]
 
 position = 50
+previous_position = copy(position)
 solution_1 = 0
+solution_2 = 0
 
 for direction, distance in moves:
     if direction == 'L':
         position -= distance
     elif direction == 'R':
         position += distance
+    if position != 100 and previous_position != 0:
+        solution_2 += abs(position // 100)
     position = position % 100
     if position == 0:
         solution_1 += 1
+        solution_2 += 1
+    previous_position = copy(position)
 
 print(solution_1)
+print(solution_2)
